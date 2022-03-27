b31ab046ecebe99163bf5e711bc07a031c28752d
Joeretz <yee.it111@gmail.com>
2022-03-27 12:07:26 +0800

    14th commit

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://final-st-jude.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
   },
 });
 export default instance;
+
+// http://127.0.0.1:8000
