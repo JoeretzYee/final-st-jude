ab51f8973983c1554b6400c761c57f2956d1ef34
Joeretz <yee.it111@gmail.com>
2022-04-08 21:48:39 +0800

    27th commit

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://final-st-jude.herokuapp.com",
+  baseURL: "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
-    xsrfCookieName: "csrftoken",
-    xsrfHeaderName: "X-CSRFToken",
   },
 });
 export default instance;
