a5b6b476e4652f165009c5796993f8a749f8bfec
Joeretz <56264637+JoeretzYee@users.noreply.github.com>
2022-04-13 13:23:47 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
+]
 CORS_ALLOW_CREDENTIALS = True
 
 
